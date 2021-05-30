f1f3f08256fd8bd3aee78c73b24f90dd2e8f5952
gparra3 <gparra@shokworks.io>
2021-05-30 18:56:57 -0400

    Starting from scratch

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
-import React, {Fragment} from 'react';
-import logo from './assets/logo.svg';
-import './App.css';
+import React from 'react';
 
-import NavBar from './components/NavBar'
-import Workplace from './components/Workplace'
 
 function App() {
 
   return (
-    <Fragment>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div className="colunm">
-          <h2 className="text-align-center">Editor de imagenes</h2>
-          <h6>Hecho por Giselt Grimilda y Denis Adrián</h6>
-        </div>
-      </header>
-
-      <NavBar/>
-      <Workplace/>
-    </div>
-    
-    </Fragment>
+    <>
+    </>
   );
 }
 
