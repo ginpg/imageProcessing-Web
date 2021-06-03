a585313a181f52923979c3c09ec134df341c1db7
gparra3 <gparra@shokworks.io>
2021-06-03 18:33:22 -0400

    Changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ import { Container } from 'react-bootstrap'
 import './estilos/estilos.css'
 import G from './assets/G.jpg'
 
+//iconos
+import ORIGINAL from './assets/icons/ORIGINAL.png'
+import BW from './assets/icons/BW.png'
+import GREY from './assets/icons/GREY.png'
+import NEG from './assets/icons/NEG.png'
+import ROTL from './assets/icons/rotL.png'
+import ROTR from './assets/icons/rotR.png'
+
+
 function App() {
       //Extraer imagen
       const[preview, setPreview] = useState('');
@@ -20,28 +29,72 @@ function App() {
   return (
     <>
       <header className="header">
-          BESTIA VALE
+        <Container>
+          <h1>Editor de Imágenes</h1>
+          <h5>Elaborado por Denis y Giselt</h5>
+        </Container>
+          
       </header>
 
       <nav className="nav">
-            <input type="file" id="foto" accept=".jpg, .jpeg, .png" onClick={extractFile}/>
-            <button type="button">Guardar</button>
+          <Container className="nav-container">
+            {/*<a><h5>Home</h5></a>*/}
+          </Container>
       </nav>
 
       <hr/>
 
       <Container className="app">
+
         <Container className="workspace">
             
-            {preview===''
-              ? <img src={G} className="imagen" width="500" height="500" alt="imagen"/>
-              : <img src={preview} className="imagen" width="500" height="500" alt="imagen"/>
-            }
-          
-            <Container className="menubar"></Container>
-          
+            <Container className="contenedor-contenido">
+
+              <Container className="side-menu">
+                <div className="inside-side-menu">
+                  <label>Iluminación</label>
+                  <input type="range" min="0" max="100" className="slider"/>
+                  <label>Contraste</label>
+                  <input type="range" min="0" max="100" className="slider"/>
+                  <button type="button" className="btn btn-outline-light">Guardar</button>
+                </div>
+                
+              </Container>
+
+              {preview===''
+                ? <img src={G} className="imagen"  height="500" alt="imagen"/>
+                : <img src={preview} className="imagen" height="500" alt="imagen"/>
+              }
+
+            </Container>
+
+            <div className="file-selector">
+                <input  type="file" id="foto" accept=".jpg, .jpeg, .png" onClick={extractFile}/>
+            </div>
+
+            <Container className="menubar">
+                  <img src={ORIGINAL} width="150" height="150" className="filter-button" alt="original"/>
+                  <img src={BW} width="150" height="150" className="filter-button" alt="blackwhite" />
+                  <img src={GREY} width="150" height="150" className="filter-button" alt="grey"/>
+                  <img src={NEG} width="150" height="150" className="filter-button" alt="negative"/>
+                  <img src={ROTL} width="150" height="150" className="filter-button" alt="rotacion-izq"/>
+                  <img src={ROTR} width="150" height="150" className="filter-button" alt="rotacion-der"/>
+
+                  <img src={ORIGINAL} width="150" height="150" className="filter-button" alt="original"/>
+                  <img src={BW} width="150" height="150" className="filter-button" alt="blackwhite" />
+                  <img src={GREY} width="150" height="150" className="filter-button" alt="grey"/>
+                  <img src={NEG} width="150" height="150" className="filter-button" alt="negative"/>
+                  <img src={ROTL} width="150" height="150" className="filter-button" alt="rotacion-izq"/>
+                  <img src={ROTR} width="150" height="150" className="filter-button" alt="rotacion-der"/>
+            </Container>        
         </Container>
+
+        <Container className="second-container">
+        <p>Hola</p>
       </Container>
+
+      </Container>
+
     </>
   );
 }
